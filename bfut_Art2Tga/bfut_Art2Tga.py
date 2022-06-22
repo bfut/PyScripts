6c4188eb9c767520f7a80689b53951e41ceb6c39
Benjamin Futász <34510704+bfut@users.noreply.github.com>
2022-06-22 15:28:14 +0200

    Change: [Art2Tga] improvements

--- bfut_Art2Tga/bfut_Art2Tga.py
+++ bfut_Art2Tga/bfut_Art2Tga.py
@@ -22,50 +22,55 @@ import argparse
 import os
 import pathlib
 
-# Parse command (or print module help)
-parser = argparse.ArgumentParser()
-parser.add_argument("cmd", nargs=1, help="path")
-args = parser.parse_args()
+def main():
+    # Parse command (or print module help)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cmd", nargs=1, help="path")
+    args = parser.parse_args()
 
-script_path = pathlib.Path(__file__).parent
-input_file = pathlib.Path(args.cmd[0])
-output_path = pathlib.Path(input_file.parent / input_file.stem)
+    input_file = pathlib.Path(args.cmd[0])
+    output_path = pathlib.Path(input_file.parent / input_file.stem)
 
 
-# --------------------------------------
-try:
-    os.mkdir(output_path)
-except FileExistsError:
-    if not output_path.is_dir():
-        raise FileExistsError("output_path exists and is not a directory")
+    # --------------------------------------
+    try:
+        os.mkdir(output_path)
+    except FileExistsError:
+        if not output_path.is_dir():
+            raise FileExistsError(
+                f"output_path exists and is not a directory ({output_path})")
 
-TGA_header1 = b'\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-TGA_header2 = b'\x20\x28'
-TGA_footer = b'\x00\x00\x00\x00\x00\x00\x00\x00TRUEVISION-XFILE.\x00'
+    tga_header1 = b"\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+    tga_header2 = b"\x20\x28"
+    tga_footer = b"\x00\x00\x00\x00\x00\x00\x00\x00TRUEVISION-XFILE.\x00"
 
-print("File =", input_file, flush=True)
-with open(input_file, mode='rb') as f:
-    buf = f.read()
-# print(len(buf), flush=True)
+    print(f"File = {input_file}", flush=True)
+    with open(input_file, mode="rb") as f:
+        buf = f.read()
+    # print(len(buf), flush=True)
 
-offset = 0
-i = 0
-while offset < len(buf):
-    TGA_width = int.from_bytes(buf[offset:offset + 0x4], "little")
-    TGA_height = int.from_bytes(buf[offset + 0x4:offset + 0x8], "little")
-    TGA_data_size = 4 * TGA_width * TGA_height
-    print(TGA_width, TGA_height)
+    offset = 0
+    i = 0
+    while offset < len(buf):
+        tga_width = int.from_bytes(buf[offset:offset + 0x4], "little")
+        tga_height = int.from_bytes(buf[offset + 0x4:offset + 0x8], "little")
+        tga_data_size = 4 * tga_width * tga_height
+        print(tga_width, tga_height)
 
-    # print(TGA_width, TGA_height, TGA_data_size, output_path, offset, flush=True)
-    with open(pathlib.Path(output_path / (format(i, '04d') + ".tga")), mode='wb') as f:
-        f.write(TGA_header1)
-        f.write(TGA_width.to_bytes(2, "little"))
-        f.write(TGA_height.to_bytes(2, "little"))
-        f.write(TGA_header2)
-        f.write(buf[offset + 0x8:offset + 0x8 + TGA_data_size])
-        f.write(TGA_footer)
-    print("wrote output file {}".format(pathlib.Path(output_path / (format(i, '04d') + ".tga"))), flush=True)
+        # print(tga_width, tga_height, tga_data_size, output_path, offset, flush=True)
+        with open(pathlib.Path(
+            output_path / (format(i, "04d") + ".tga")), mode="wb") as f:
+            f.write(tga_header1)
+            f.write(tga_width.to_bytes(2, "little"))
+            f.write(tga_height.to_bytes(2, "little"))
+            f.write(tga_header2)
+            f.write(buf[offset + 0x8:offset + 0x8 + tga_data_size])
+            f.write(tga_footer)
+        print(f"wrote file {pathlib.Path(output_path / format(i, '04d')).with_suffix('.tga')}", flush=True)
 
-    offset += 0x8 + TGA_data_size
-    i += 1
-print("extracted {} TGA files".format(i), flush=True)
+        offset += 0x8 + tga_data_size
+        i += 1
+    print(f"extracted {i} TGA files", flush=True)
+
+if __name__ == "__main__":
+    main()
--- bfut_Art2Tga/bfut_Tga2Art.py
+++ bfut_Art2Tga/bfut_Tga2Art.py
@@ -21,48 +21,52 @@
 import argparse
 import pathlib
 
-# Parse command (or print module help)
-parser = argparse.ArgumentParser()
-parser.add_argument("cmd", nargs=1, help="path")
-args = parser.parse_args()
+def main():
+    # Parse command (or print module help)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cmd", nargs=1, help="path")
+    args = parser.parse_args()
 
-script_path = pathlib.Path(__file__).parent
-input_folder = pathlib.Path(args.cmd[0])
+    script_path = pathlib.Path(__file__).parent
+    input_folder = pathlib.Path(args.cmd[0])
 
 
-# --------------------------------------
-output_path = pathlib.Path(script_path / (input_folder.name + ".art"))
+    # --------------------------------------
+    output_path = pathlib.Path(script_path / input_folder.name).with_suffix(".art")
 
-i = 0
-with open(output_path, mode='wb') as f:
-    for x in sorted(input_folder.glob("*.[tT][gG][aA]")):
-        print("File =", x)
-        with open(x, mode='rb') as f2:
-            buf = f2.read()
+    i = 0
+    with open(output_path, mode="wb") as f:
+        for x in sorted(input_folder.glob("*.[tT][gG][aA]")):
+            print(f"File = {x}")
+            with open(x, mode="rb") as f2:
+                buf = f2.read()
 
-        TGA_id_length = int(buf[0])
-        TGA_width = int.from_bytes(buf[12:14], "little")
-        TGA_height = int.from_bytes(buf[14:16], "little")
-        TGA_data_size = 4 * TGA_width * TGA_height
-        hdr_len = 0x12 + TGA_id_length
+            tga_id_length = int(buf[0])
+            tga_width = int.from_bytes(buf[12:14], "little")
+            tga_height = int.from_bytes(buf[14:16], "little")
+            tga_data_size = 4 * tga_width * tga_height
+            hdr_len = 0x12 + tga_id_length
 
-        if len(buf) < TGA_data_size:
-            print("TGA format error (missing alpha channel?)")
-            continue
+            if len(buf) < tga_data_size:
+                print("TGA format error (missing alpha channel?)")
+                continue
 
-        # print("header:", buf[:hdr_len], flush=True)
-        # # print(buf[hdr_len:hdr_len + TGA_data_size])
-        # print("footer:", buf[hdr_len + TGA_data_size:], flush=True)
-        print(TGA_id_length, TGA_width, TGA_height, TGA_data_size, len(buf), flush=True)
-        # # print(TGA_width.to_bytes(4, "little"))
-        # # print(TGA_height.to_bytes(4, "little"))
+            # print("header:", buf[:hdr_len], flush=True)
+            # # print(buf[hdr_len:hdr_len + tga_data_size])
+            # print("footer:", buf[hdr_len + tga_data_size:], flush=True)
+            print(tga_id_length, tga_width, tga_height, tga_data_size, len(buf), flush=True)
+            # # print(tga_width.to_bytes(4, "little"))
+            # # print(tga_height.to_bytes(4, "little"))
 
-        f.write(TGA_width.to_bytes(4, "little"))
-        f.write(TGA_height.to_bytes(4, "little"))
-        f.write(buf[hdr_len:hdr_len + TGA_data_size])
+            f.write(tga_width.to_bytes(4, "little"))
+            f.write(tga_height.to_bytes(4, "little"))
+            f.write(buf[hdr_len:hdr_len + tga_data_size])
 
-        i += 1
+            i += 1
 
-print("wrote output file {} (contains {} TGA files)".format(output_path, i), flush=True)
-if i < 1:
-    print("Warning: empty ART archive")
+    print(f"wrote file {output_path} (contains {i} TGA files)", flush=True)
+    if i < 1:
+        print("Warning: empty ART archive")
+
+if __name__ == "__main__":
+    main()

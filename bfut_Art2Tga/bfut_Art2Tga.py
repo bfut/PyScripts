3e024b37fa05d7e3df88ac7533ad509ef17a96b9
Benjamin Futász <34510704+bfut@users.noreply.github.com>
2022-06-28 09:07:38 +0200

    Change: [Art2Tga] improvements

--- bfut_Art2Tga/bfut_Art2Tga.py
+++ bfut_Art2Tga/bfut_Art2Tga.py
@@ -1,76 +1,72 @@
 """
-    bfut_Art2Tga.py - from given ART archive unpack TGA data to subfolder
-    Copyright (C) 2021 and later Benjamin Futasz <https://github.com/bfut>
+bfut_Art2Tga.py - from given ART archive unpack TGA data to subfolder
+Copyright (C) 2021 and later Benjamin Futasz <https://github.com/bfut>
 
-    This software is provided 'as-is', without any express or implied
-    warranty.  In no event will the authors be held liable for any damages
-    arising from the use of this software.
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
 
-    Permission is granted to anyone to use this software for any purpose,
-    including commercial applications, and to alter it and redistribute it
-    freely, subject to the following restrictions:
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
 
-    1. The origin of this software must not be misrepresented; you must not
-        claim that you wrote the original software. If you use this software
-        in a product, an acknowledgment in the product documentation would be
-        appreciated but is not required.
-    2. Altered source versions must be plainly marked as such, and must not be
-        misrepresented as being the original software.
-    3. This notice may not be removed or altered from any source distribution.
+1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
 """
 import argparse
 import os
 import pathlib
 
 def main():
-    # Parse command (or print module help)
+    # Parse command-line
     parser = argparse.ArgumentParser()
-    parser.add_argument("cmd", nargs=1, help="path")
+    parser.add_argument("path", nargs=1, help="file path")
     args = parser.parse_args()
 
-    input_file = pathlib.Path(args.cmd[0])
-    output_path = pathlib.Path(input_file.parent / input_file.stem)
-
+    input_file = pathlib.Path(args.path[0])
+    output_path = input_file.parent / input_file.stem
 
     # --------------------------------------
-    try:
+    if not output_path.exists():
         os.mkdir(output_path)
-    except FileExistsError:
-        if not output_path.is_dir():
-            raise FileExistsError(
-                f"output_path exists and is not a directory ({output_path})")
+    elif not output_path.is_dir():
+        raise FileExistsError("output_path exists "
+                              f"and is not a directory ('{output_path}')")
 
     tga_header1 = b"\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00"
     tga_header2 = b"\x20\x28"
     tga_footer = b"\x00\x00\x00\x00\x00\x00\x00\x00TRUEVISION-XFILE.\x00"
 
     print(f"File = {input_file}", flush=True)
-    with open(input_file, mode="rb") as f:
-        buf = f.read()
-    # print(len(buf), flush=True)
-
+    buf = input_file.read_bytes()
     offset = 0
     i = 0
     while offset < len(buf):
         tga_width = int.from_bytes(buf[offset:offset + 0x4], "little")
         tga_height = int.from_bytes(buf[offset + 0x4:offset + 0x8], "little")
         tga_data_size = 4 * tga_width * tga_height
-        print(tga_width, tga_height)
 
-        # print(tga_width, tga_height, tga_data_size, output_path, offset, flush=True)
-        with open(pathlib.Path(
-            output_path / (format(i, "04d") + ".tga")), mode="wb") as f:
+        print(f"w={tga_width} h={tga_height}")
+        # print(tga_width, tga_height, tga_data_size, output_path, offset)
+        with open(
+            (output_path / format(i, "04d")).with_suffix(".tga"), mode="wb"
+        ) as f:
             f.write(tga_header1)
             f.write(tga_width.to_bytes(2, "little"))
             f.write(tga_height.to_bytes(2, "little"))
             f.write(tga_header2)
             f.write(buf[offset + 0x8:offset + 0x8 + tga_data_size])
             f.write(tga_footer)
-        print(f"wrote file {pathlib.Path(output_path / format(i, '04d')).with_suffix('.tga')}", flush=True)
+        print(f"created file {(output_path / f'{i:04d}').with_suffix('.tga')}")
 
         offset += 0x8 + tga_data_size
         i += 1
-    print(f"extracted {i} TGA files", flush=True)
+    print(f"extracted {i} TGA files")
 
 if __name__ == "__main__":
     main()
--- bfut_Art2Tga/bfut_Tga2Art.py
+++ bfut_Art2Tga/bfut_Tga2Art.py
@@ -1,45 +1,46 @@
 """
-    bfut_Tga2Art.py - generates ART archive from TGA files in given folder (input files sorted)
-    Copyright (C) 2021 and later Benjamin Futasz <https://github.com/bfut>
+bfut_Tga2Art.py - generates ART archive from TGA files in given folder (input files sorted)
+Copyright (C) 2021 and later Benjamin Futasz <https://github.com/bfut>
 
-    This software is provided 'as-is', without any express or implied
-    warranty.  In no event will the authors be held liable for any damages
-    arising from the use of this software.
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
 
-    Permission is granted to anyone to use this software for any purpose,
-    including commercial applications, and to alter it and redistribute it
-    freely, subject to the following restrictions:
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
 
-    1. The origin of this software must not be misrepresented; you must not
-        claim that you wrote the original software. If you use this software
-        in a product, an acknowledgment in the product documentation would be
-        appreciated but is not required.
-    2. Altered source versions must be plainly marked as such, and must not be
-        misrepresented as being the original software.
-    3. This notice may not be removed or altered from any source distribution.
+1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
 """
 import argparse
 import pathlib
 
 def main():
-    # Parse command (or print module help)
+    # Parse command-line
     parser = argparse.ArgumentParser()
-    parser.add_argument("cmd", nargs=1, help="path")
+    parser.add_argument("path", nargs=1, help="folder path")
     args = parser.parse_args()
 
     script_path = pathlib.Path(__file__).parent
-    input_folder = pathlib.Path(args.cmd[0])
-
+    input_folder = pathlib.Path(args.path[0])
 
     # --------------------------------------
-    output_path = pathlib.Path(script_path / input_folder.name).with_suffix(".art")
+    if not input_folder.is_dir():
+        raise NotADirectoryError("input_folder does not exist "
+                                 f"or is not a directory ('{input_folder}')")
+    output_path = (script_path / input_folder.name).with_suffix(".art")
 
     i = 0
     with open(output_path, mode="wb") as f:
         for x in sorted(input_folder.glob("*.[tT][gG][aA]")):
             print(f"File = {x}")
-            with open(x, mode="rb") as f2:
-                buf = f2.read()
+            buf = x.read_bytes()
 
             tga_id_length = int(buf[0])
             tga_width = int.from_bytes(buf[12:14], "little")
@@ -48,15 +49,16 @@ def main():
             hdr_len = 0x12 + tga_id_length
 
             if len(buf) < tga_data_size:
-                print("TGA format error (missing alpha channel?)")
+                print("Skipping... TGA format error (missing alpha channel?)")
                 continue
 
-            # print("header:", buf[:hdr_len], flush=True)
-            # # print(buf[hdr_len:hdr_len + tga_data_size])
-            # print("footer:", buf[hdr_len + tga_data_size:], flush=True)
-            print(tga_id_length, tga_width, tga_height, tga_data_size, len(buf), flush=True)
-            # # print(tga_width.to_bytes(4, "little"))
-            # # print(tga_height.to_bytes(4, "little"))
+            # print("header:", buf[:hdr_len])
+            # print(buf[hdr_len:hdr_len + tga_data_size])
+            # print("footer:", buf[hdr_len + tga_data_size:])
+            print(f"id_len={tga_id_length} w={tga_width} h={tga_height} "
+                  f"in_size={tga_data_size} out_size={len(buf)}")
+            # print(tga_width.to_bytes(4, "little"))
+            # print(tga_height.to_bytes(4, "little"))
 
             f.write(tga_width.to_bytes(4, "little"))
             f.write(tga_height.to_bytes(4, "little"))
@@ -64,9 +66,9 @@ def main():
 
             i += 1
 
-    print(f"wrote file {output_path} (contains {i} TGA files)", flush=True)
+    print(f"created file {output_path} (contains {i} TGA files)")
     if i < 1:
-        print("Warning: empty ART archive")
+        raise Warning("empty ART archive")
 
 if __name__ == "__main__":
     main()
